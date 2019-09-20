7cf016ca1978821e28e2807df16ccc7db3e79fc6
unknown <mariia>
2019-09-21 02:14:09 +0300

    Updates

---
+++ precache-manifest.a664f9ab811726e9527b076a352489e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bed696f7376989956baf79318b5ee96",
+    "url": "/venbest-test/index.html"
+  },
+  {
+    "revision": "f33a2b2064afbcda55b4",
+    "url": "/venbest-test/static/css/main.cedf4bdc.chunk.css"
+  },
+  {
+    "revision": "b44c8c4817981090ad66",
+    "url": "/venbest-test/static/js/2.e912ceb5.chunk.js"
+  },
+  {
+    "revision": "f33a2b2064afbcda55b4",
+    "url": "/venbest-test/static/js/main.36c4d479.chunk.js"
+  },
+  {
+    "revision": "33850c8e793d13138cba",
+    "url": "/venbest-test/static/js/runtime~main.ec59f14b.js"
+  }
+]);
